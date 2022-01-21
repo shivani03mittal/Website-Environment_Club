1d9f893220daa9589ebdb3905631dc57586c7ae6
shivani03mittal <shivani03mittal@gmail.com>
2022-01-21 19:50:22 +0530

    final init

---
+++ src/models/donate.js
@@ -0,0 +1,24 @@
+const mongoose = require ("mongoose");
+const donationSchema = new mongoose.Schema(
+
+    {
+        firstname:{type:String},
+        lastname: {type:String},
+        mail: {type:String},
+        Inspiration: {type:String},
+        checkbox:{type:String},
+        name:{type:String},
+        cardnumber:{type:String},
+        exdate:{type:String},
+        cvv:{type:String, password:true},
+        contactno:{type:String},
+        amount:{type:String},
+        upi:{type:String},
+        transacid:{type:String},
+
+
+    }
+);
+
+const Donate = new mongoose.model("Donation Data",donationSchema);
+module.exports = Donate;
---
+++ src/models/register.js
@@ -0,0 +1,19 @@
+const mongoose = require ("mongoose");
+//reg
+
+const RegSchema = new mongoose.Schema(
+
+    {
+        rfirstname:{type:String},
+        rlastname: {type:String},
+        rmail: {type:String},
+        Contact:{type:String},
+        batch: {type:String},
+        event:{type:String},
+        suggestions:{type:String},
+    
+    }
+);
+
+const Registeration = new mongoose.model("Registeration Data",RegSchema);
+module.exports = Registeration;
